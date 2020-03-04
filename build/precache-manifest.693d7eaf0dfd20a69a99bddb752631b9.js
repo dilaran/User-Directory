4f6c0c26b4d2df500a7543b3a8b6047e29fdb22b
DN <dilaran@live.com>
2020-03-04 13:05:47 -0800

    Your awesome message

---
+++ build/precache-manifest.693d7eaf0dfd20a69a99bddb752631b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9b464b8eefad739ba27f9902154d09",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5d383d95cfaece0460e0",
+    "url": "/my-app/static/css/main.8af79651.chunk.css"
+  },
+  {
+    "revision": "8d5ddabed9e5bdde212f",
+    "url": "/my-app/static/js/2.6d060829.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-app/static/js/2.6d060829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d383d95cfaece0460e0",
+    "url": "/my-app/static/js/main.5be30c7c.chunk.js"
+  },
+  {
+    "revision": "1b58fcb5ce8d26145ee7",
+    "url": "/my-app/static/js/runtime-main.8b909c6e.js"
+  }
+]);
